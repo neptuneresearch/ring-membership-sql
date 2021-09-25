a529dc23fb0a565fb3bef44cb774ec691248806e
neptuneresearch <neptuneresearch@protonmail.com>
2021-09-25 04:00:59 +0000

    ring_lock_invalid: build fix

--- views/ring_lock_invalid.sql
+++ views/ring_lock_invalid.sql
@@ -1,8 +1,6 @@
-DECLARE block_height_HF_VERSION_ENFORCE_MIN_AGE INT = 1978433;
-DECLARE CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE INT = 10;
-
 SELECT
 	COUNT(1)
 FROM txo_first_ring
-WHERE ring_block_height >= block_height_HF_VERSION_ENFORCE_MIN_AGE
-	AND ring_block_height - txo_block_height < CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE;
+WHERE 
+	ring_block_height >= 1978433 -- Block height for HF_VERSION_ENFORCE_MIN_AGE
+	AND ring_block_height - txo_block_height < 10; -- CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE
